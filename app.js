dad1af3d0e3cb5f7defb73978a02a76fb4b11ffd
Efe Han Oz <159789584+wlanHan@users.noreply.github.com>
2024-05-20 15:24:27 +0300

    Update app.js

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const generateForm = document.querySelector(".generate-form");
 const generateBtn = generateForm.querySelector(".generate-btn");
 const imageGallery = document.querySelector(".image-gallery");
 
-const OPENAI_API_KEY = "sk-proj-8VL3UwvE6BcK4kzDJyk8T3BlbkFJGqUUSDH0cKFhklbCyvs1"; // OpenAI API Buraya
+const OPENAI_API_KEY = "sk-proj-8VL3UwvE6BcK4kzDJyk8-api-lbkFJGqUUSDH0cKFhklbCyvs1"; // OpenAI API Buraya
 let isImageGenerating = false;
 
 const updateImageCard = (imgDataArray) => {
